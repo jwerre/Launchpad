382ec072e28c5619f9270a32bd0c30ca429a3f61
Jonah Werre <jonahwerre@gmail.com>
2011-06-06 19:07:51 -0700

    uncommented TINY

--- admin/js/plugins.js
+++ admin/js/plugins.js
@@ -725,24 +725,24 @@ $(function() {
 //WYSIWIG EDITOR (tiny editor)
 if( $('#content_body').length > 0 ){
 	
-	// new TINY.editor.edit('editor',{
-	// 	id:'content_body',
-	// 	width:'100%',
-	// 	height:200,
-	// 	cssclass:'text_edit',
-	// 	controlclass:'text_edit_control',
-	// 	rowclass:'text_edit_header',
-	// 	dividerclass:'text_edit_divider',
-	// 	controls:['bold','italic','underline','strikethrough','|','subscript','superscript','|','orderedlist','unorderedlist','|','outdent','indent','|','leftalign','centeralign','rightalign','blockjustify','|','unformat','|','undo','redo','n',/*'font','size',*/'style','|','image','hr','link','unlink','|','cut','copy','paste','print'],
-	// 	footer:true,
-	// 	fonts:['Verdana','Arial','Georgia','Trebuchet MS'],
-	// 	xhtml:true,
-	// 	cssfile:'style.css',
-	// 	bodyid:'editor',
-	// 	footerclass:'text_edit_footer',
-	// 	toggle:{text:'source',activetext:'wysiwyg',cssclass:'toggle'},
-	// 	resize:{cssclass:'resize'}
-	// });
+	new TINY.editor.edit('editor',{
+		id:'content_body',
+		width:'100%',
+		height:200,
+		cssclass:'text_edit',
+		controlclass:'text_edit_control',
+		rowclass:'text_edit_header',
+		dividerclass:'text_edit_divider',
+		controls:['bold','italic','underline','strikethrough','|','subscript','superscript','|','orderedlist','unorderedlist','|','outdent','indent','|','leftalign','centeralign','rightalign','blockjustify','|','unformat','|','undo','redo','n',/*'font','size',*/'style','|','image','hr','link','unlink','|','cut','copy','paste','print'],
+		footer:true,
+		fonts:['Verdana','Arial','Georgia','Trebuchet MS'],
+		xhtml:true,
+		cssfile:'style.css',
+		bodyid:'editor',
+		footerclass:'text_edit_footer',
+		toggle:{text:'source',activetext:'wysiwyg',cssclass:'toggle'},
+		resize:{cssclass:'resize'}
+	});
 	
 }
 // var $tab_items = $( 'ul:first li', $tabs ).droppable({
